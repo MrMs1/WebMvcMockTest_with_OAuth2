07c836947fedf8fbf3d40e53d5f3fc64ecf40f86
MrMs1 <46705152+MrMs1@users.noreply.github.com>
2022-04-27 22:52:39 +0900

    ソースコード追加

---
+++ springsecurityoauth2demo/src/test/java/com/example/springsecurityoauth2demo/controller/SampleRestControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.springsecurityoauth2demo.controller;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(SampleRestController.class)
+class SampleRestControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
+
+	@Test
+	@WithMockUser(roles = "access")
+	void accessロールを持つリクエストは許可される() throws Exception {
+		mockMvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("hello")));
+	}
+
+}
